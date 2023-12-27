ef2bff2b12c526a554e7eed1b998d116b39c57b8
Asael Hernandez <zanduva@gmail.com>
2023-12-26 22:33:20 -0600

    landing advance

--- src/components/landing/LandingPage.jsx
+++ src/components/landing/LandingPage.jsx
@@ -1,10 +1,69 @@
 import React from "react";
-import "./LandingPage.css"
+import cobague from "../../assets/cobague.png";
+import konstr from "../../assets/konstructor.png";
+import setroc from "../../assets/SETROC.png";
+import "./LandingPage.css";
+import SelectComponent from "../styledComponents/select/SelectComponent";
+import { Button } from "@mui/material";
 
 export const LandingPage = () => {
-    return (
-        <div className="bg-image h-screen flex flex-col justify-center items-center">
-
+  return (
+    <div className="bg-image h-screen flex flex-col justify-center items-center">
+      <div className=" bg-orange-200 h-full w-full bg-opacity-30 flex flex-col justify-center items-center gap-6 p-10">
+        <div className="grid grid-cols-2 md:flex flex-row md:justify-evenly place-items-center gap-12 items-center w-full md:relative md:bottom-32">
+          <img
+            src={konstr}
+            alt="cobague"
+            className="grupo1"
+          />
+          <img
+            src={cobague}
+            alt="cobague"
+            className="grupo1"
+          />
+          <img
+            src={setroc}
+            alt="cobague"
+            className="grupo2"
+          />
         </div>
-    )
-}
+        <h1 className="title text-7xl text-center md:text-9xl">
+          El terreno a tu medida
+        </h1>
+        <div className="w-full md:w-8/12 flex flex-col md:flex-row gap-5 bg-smoke p-4 rounded-lg drop-shadow-2xl">
+          <SelectComponent
+            items={[
+              "San Andres, Cuauhtempan",
+              "Tlayacapan, Morelos",
+              'Lomas "El Potrero"',
+              "Totoloapan, Morelos",
+              "Ahuatlan, Morelos",
+            ]}
+            title="Localidad"
+            name="terrenos"
+            key={1}
+          />
+          <SelectComponent
+            items={["Terreno", "Inmueble"]}
+            title="Tipo de propiedad"
+            name="terrenos"
+            key={2}
+          />
+          <SelectComponent
+            items={["Zacanco", "Piedra Rosa", "Salida"]}
+            title="Encuentra tu terreno"
+            name="terrenos"
+            key={3}
+          />
+
+          <Button
+            className="md:w-3/12 transition-all ease-in-out hover:transition-all hover:ease-in-out hover:scale-110 "
+            variant="contained"
+          >
+            Buscar
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
