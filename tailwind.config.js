2354da0be7cb0676063fb3d03b4842ccfe5abe09
Asael Hernandez <zanduva@gmail.com>
2023-12-26 19:20:18 -0600

    change

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    // Añade más rutas o patrones según sea necesario
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
